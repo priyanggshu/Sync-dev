b2d8b0a1c8ae01d0f969eb247672a7ecd125c000
Priyangshu <shomepriyangshu16@gmail.com>
2025-02-12 11:42:30 +0530

    initial commit for the client dir

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "./"
 
+export 
 const ProtectedRoute = () => {
   return (
     <div>ProtectedRoute</div>
